f78de1eaa39e5b137b74416bf6fbce0f0a5c690e
Geovanna <geovannaoliveira169@gmail.com>
2022-04-13 11:21:00 -0300

    ProjetoBanco

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Conta c1 = new Conta(1);
+Conta c2 = new Conta(2);
+
+c1.Depositar(1000.00);
+c1.Sacar(200.00);
+Console.WriteLine(c1.Codigo);
+Console.WriteLine(c1.Saldo);
+
+c2.Depositar(200.00);
+c2.Sacar(50.00);
+Console.WriteLine(c2.Codigo);
+Console.WriteLine(c2.Saldo);
+
